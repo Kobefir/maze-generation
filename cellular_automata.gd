86df1ef4b37904575dbcf07629beb4f007fe0ca0
Kobe Shimoyama <kobefir@gmail.com>
2025-09-18 22:09:26 -0700

    Started cellular automata class definition
    - Added inner class Cell which will represent each automaton in the maze "board"

--- cellular_automata.gd
+++ cellular_automata.gd
@@ -1,1 +1,15 @@
-extends RefCounted
+class_name CellularAutomata extends Node2D
+
+enum CellState {
+	DISCONNECTED,
+	SEED,
+	INVITE,
+	CONNECTED
+}
+
+class Cell:
+	var state := CellState.DISCONNECTED
+	var connect_vector : int
+	
+	# TODO: the article
+	
