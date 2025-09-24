e4fa86f0fee4608f222dcd556090b80a3fa6b018
Kobe Shimoyama <kobefir@gmail.com>
2025-09-23 20:45:05 -0700

    Minor additions to cellular_automata.gd

--- cellular_automata.gd
+++ cellular_automata.gd
@@ -1,6 +1,14 @@
 class_name CellularAutomata extends Node2D
 
+# Maze dimensions in cell (junction) counts
+@export var maze_width: int = 16 
+@export var maze_height: int = 16
+
+@export var wall_thickness: int = 1
+@export var floor_thickness: int = 1
 @export var branch_probability: int = 5	# Percent chance of branching
+
+var maze: Dictionary[Vector2i, Cell]
 
 enum CellState {
 	DISCONNECTED,
